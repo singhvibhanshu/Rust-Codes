3cd3db5ea8c47cd1aa8520608c1ce924aa4131de
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2024-12-22 19:23:05 +0530

    test

--- playing_with_rust.rs
+++ playing_with_rust.rs
@@ -1,5 +1,7 @@
 fn main(){
-    let x = 4;
-    //x = "Hello!"; //this can't be done in Rust but can be done in Python. 
+    let mut x = 4;
+    println!("x is: {}", x);
+    //x = "Hello!"; //this can't be done in Rust but can be done in Python.
+    let x = 5;
     println!("x is: {}", x);
 }
